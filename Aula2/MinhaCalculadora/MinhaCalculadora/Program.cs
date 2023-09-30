db98e728d74920d74ed34cce33f4344b140526db
Ricardo Paulino do Nascimento <ricardo.paulino.nascimento@gmail.com>
2023-09-30 17:00:49 -0300

    soma

---
+++ Aula2/MinhaCalculadora/MinhaCalculadora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MinhaCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Soma(2, 3));
+            Console.ReadKey();
+        }
+        // entrada - processamento - saída
+        public static int Soma(int a, int b)
+        {
+            var funcao = a + b;
+            return funcao;
+        }
+        public static int Subtracao(int a, int b)
+        {
+            return;
+        }
+
+    }
+}
